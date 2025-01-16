7a3e568337f85fffd1e8787ca4a46f5333f39ea8
DiogoVieira123 <1240448@isep.ipp.pt>
2025-01-16 14:07:00 +0100

    Add the container image for the api service
    First working version of the image for the public api. It builds and it
    serves, the layer layout is something we will look at later.

---
+++ services/api/src/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const http = require('node:http');
+
+const port = Number(process.env.PORT || 3000);
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/healthz') {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+    return;
+  }
+
+  res.writeHead(404);
+  res.end();
+});
+
+server.listen(port);
